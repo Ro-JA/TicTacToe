e3f41884099a96a6d58be1f92987f9956c378eb8
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-03-30 12:11:25 +0900

    correct two

--- src/main/kotlin/30_03_23.kt
+++ src/main/kotlin/30_03_23.kt
@@ -2,10 +2,10 @@
 data class Box2(val height: Int, val length: Int, val width: Int)
 
 fun main() {
-    val inputParam = IntArray(4) { readln().toInt() }
+    val(height, length1, length2, width) = IntArray(4) { readln().toInt() }
 
-    val box1 = Box2(inputParam[0], inputParam[1], inputParam[3])
-    val box2 = box1.copy(length = inputParam[2])
+    val box1 = Box2(height, length1, width)
+    val box2 = box1.copy(length = length2)
 
     println(box1)
     println(box2)
