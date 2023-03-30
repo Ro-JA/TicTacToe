b671b84a1d127af7266d6ca99079bc0359796695
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-03-30 10:10:03 +0900

    new day

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,3 @@
 fun main(args: Array<String>) {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
-    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-    println(question1.toString())
-
-    println("${StudentProgress.answer} of ${StudentProgress.total}answer.")
+   println("${Quiz.answer} of ${Quiz.total} answered")
 }
