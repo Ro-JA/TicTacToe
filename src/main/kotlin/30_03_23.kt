82151ba88d00c2b958f0ebae51c7418f8ca9d9bf
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-03-30 12:49:04 +0900

    Add an extension property

--- src/main/kotlin/30_03_23.kt
+++ src/main/kotlin/30_03_23.kt
@@ -1,4 +1,5 @@
 // Повторение дата класса
+/*
 data class Box2(val height: Int, val length: Int, val width: Int)
 
 fun main() {
@@ -9,4 +10,32 @@ fun main() {
 
     println(box1)
     println(box2)
+}
+*/
+
+fun main() {
+    Quiz.printProgressBar()
+}
+
+//создадим класс викторина для соопутствующего обекта
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    // класс с одним экземпляром обекта singleton
+    companion object StudentProgress {
+        val total: Int = 10
+        val answer: Int = 3
+    }
+}
+
+// Определим свойства расширения
+val Quiz.StudentProgress.progressText: String
+    get() = "${answer} of ${total} answered" // свойства раштрения не могут хранит даный по этому есть только гетер
+
+// Создадим функцию с раширением викторина не должна принимать параметры  и нечего не возрощает
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answer) {print("▓") }
+    repeat(Quiz.total - Quiz.answer) { print( "▒")}
 }
