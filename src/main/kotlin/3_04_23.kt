604720012f26c733e68316d20a00364b5937af65
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-04 09:27:47 +0900

    new day

--- src/main/kotlin/3_04_23.kt
+++ src/main/kotlin/3_04_23.kt
@@ -5,5 +5,16 @@
     val newList = firstList + secondList
     println(newList.joinToString())
 }*/
-
+fun main() {
+    val list = MutableList(readln().toInt()) { readln().toInt() }
+    val route = readln().toInt() % list.size
+    if (route == 0) {
+        println(list.joinToString(" "))
+    } else {
+        for (i in 1..route) {
+            list.add(0, list.removeLast())
+        }
+        println(list.joinToString(" "))
+    }
+}
 fun solution(strings: List<String>, str: String): Int = strings.count { it == str }
