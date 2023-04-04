63b5c9a3e65d10f6902188e38bab26582f630592
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-04 11:02:49 +0900

    add set

--- src/main/kotlin/4_04_23.kt
+++ src/main/kotlin/4_04_23.kt
@@ -1,6 +1,5 @@
-/*
 // проблема дня с hyperskill вторичные конструкторы
-class EspressoMachine() {
+class EspressoMachine1() {
     var costPerServing: Float = 0.0f
 
     constructor(
@@ -16,10 +15,10 @@ class EspressoMachine() {
         this.costPerServing = totalCost / coffeeBeansWeight / 10
     }
 }
-*/
+
 
 // новая реализация вторичных конструкторов через data class
-data class EspressoMachine(var costPerServing: Float = 0.0f) {
+data class EspressoMachine2(var costPerServing: Float = 0.0f) {
     companion object {
         const val BEANS_PER_CUP = 10
     }
@@ -27,4 +26,15 @@ data class EspressoMachine(var costPerServing: Float = 0.0f) {
     constructor(coffeeCapsulesCount: Int, totalCost: Float) : this(totalCost / coffeeCapsulesCount)
     constructor(coffeeBeansWeight: Float, totalCost: Float) : this(totalCost / (coffeeBeansWeight / BEANS_PER_CUP))
 }
+
+
+// работа с set списками
+// возрощает set список из mutableList списка
+fun solution(strings: MutableList<String>): Set<String> = strings.toSet()
+//Обеденяет два set списка в один
+fun solution(first: Set<String>, second: Set<String>): Set<String> = first + second
+//Возрощает true если есть элементы из изменяемого списка
+fun solution(first: Set<String>, second: MutableList<String>): Boolean = first.containsAll(second)
+
+// работа со списками
 
