1f663f8d58c51e9f82be503112114ef28400de19
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-07 14:41:44 +0900

    add map

--- src/main/kotlin/7_04_23.kt
+++ src/main/kotlin/7_04_23.kt
@@ -1,2 +1,21 @@
 // фнукция возрощает set список
 fun intsToSet(ints: MutableList<Int>): Set<Int> = ints.toSet()
+// работа с map
+fun main() {
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+
+    println(solarSystem.size)
+    solarSystem["Pluto"] = 5
+    println(solarSystem.size)
+    println(solarSystem["Pluto"])
+    println(solarSystem["Their"])
+}
