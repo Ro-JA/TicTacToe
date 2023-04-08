47cdf49f44e06365e27e0d2ffca3a5c4db159815
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-08 21:49:54 +0900

    add mutableList

---
+++ src/main/kotlin/8_04_23.kt
@@ -0,0 +1,30 @@
+/*
+// програма по свойствам выдает самый холдный город
+class City(val name: String) {
+    var degrees: Int = 5
+        set(value) {
+            if(value in -92..57)
+        }
+}
+
+fun main() {
+    val first = readLine()!!.toInt()
+    val second = readLine()!!.toInt()
+    val third = readLine()!!.toInt()
+    val firstCity = City("Dubai")
+    firstCity.degrees = first
+    val secondCity = City("Moscow")
+    secondCity.degrees = second
+    val thirdCity = City("Hanoi")
+    thirdCity.degrees = third
+
+    //implement comparing here
+
+
+}*/
+/*
+// повторение mutableList
+fun main() {
+    val characters = mutableListOf('a', 'z', 'e', 'd')
+    println(characters.joinToString())
+}*/
