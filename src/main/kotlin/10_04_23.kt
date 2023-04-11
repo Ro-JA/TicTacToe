0f6a9f7df331f47e6dd698cff898fda862b8bfbc
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-11 10:29:48 +0900

    new day

--- src/main/kotlin/10_04_23.kt
+++ src/main/kotlin/10_04_23.kt
@@ -1,6 +1,8 @@
+/*
 //сортировка пузыркем
 fun main() {
     val array = intArrayOf(1,5,3,9,10,2,4,6,8,11,7)
+    array.sort()
     println(array.toList())
     array.bubbleSort()
     println(array.toList())
@@ -26,3 +28,35 @@ private fun IntArray.swap(index1: Int, index2: Int) {
     this[index1] = this[index2]
     this[index2] = buffer
 }
+*/
+/*
+// повторение свойств класса
+class City(val name: String) {
+    var degrees: Int = 0
+        set(value) {
+            if(value < -92 && value > 57) {
+                field = when (name) {
+                    "Dubai" -> 30
+                    "Moscow" -> 5
+                    "Hanoi" -> 20
+                    else -> 0
+                }
+            }
+        }
+}
+
+fun main() {
+    val first = readLine()!!.toInt()
+    val second = readLine()!!.toInt()
+    val third = readLine()!!.toInt()
+    val firstCity = City("Dubai")
+    firstCity.degrees = first
+    val secondCity = City("Moscow")
+    secondCity.degrees = second
+    val thirdCity = City("Hanoi")
+    thirdCity.degrees = third
+
+    val minTemperate = mutableListOf<City>(firstCity, secondCity, thirdCity)
+    println(minTemperate.minOfOrNull { it.degrees })
+}
+*/
