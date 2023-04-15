4343d25db01ffe69b9e24026db294571a69bf2b9
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-15 11:59:59 +0900

    correct two

--- src/main/kotlin/15_04_23.kt
+++ src/main/kotlin/15_04_23.kt
@@ -5,6 +5,11 @@ fun main() {
     solution(products, product)
 }
 
+/*
+    выводим индекс элемента равный элементу заданому, берем списо products через функцию indices берем индекс который
+    через функцию filter равен задоному элементу, через joinToString добовляем пробел и печатаем
     fun solution(products: List<String>, product: String) {
        print(products.indices.filter { products[it] == product }.joinToString(" "))
-    }
+    }*/
+//через цикл forEachIndexed проходим по индексам а и элеметам s  сравневаем  элемент с заданым значением и печатаем индекс
+fun solution(ps: List<String>, p: String) = ps.forEachIndexed{ a, s -> if(s == p) print("$a ") }
