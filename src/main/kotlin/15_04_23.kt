dc3a391b7718457ce51fb532a0bf03bf925c2cf7
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-15 11:41:01 +0900

    work

--- src/main/kotlin/15_04_23.kt
+++ src/main/kotlin/15_04_23.kt
@@ -1,18 +1,16 @@
 //повторение List
 fun main() {
-    val products = listOf<String>("Mustard", "Cheese", "Eggs", "Cola", "Eggs" )
+    val products = listOf<String>("Mustard", "Cheese", "Eggs", "Cola", "Eggs")
     val product = "Eggs"
     solution(products, product)
 }
 
-fun solution(products: List<String>, product: String) {
-    if (products.isNotEmpty()) {
+    fun solution(products: List<String>, product: String) {
         if (product.contains(product)) {
-            for (i in products) {
-                if (i == product) {
-                    print("${products.indexOf(product)} ")
+            for (index in products.indices) {
+                if (products[index] == product) {
+                    print("$index ")
                 }
             }
         }
     }
-}
