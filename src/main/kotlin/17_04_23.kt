b58cfabfd9c231ca74b434bf84793a4ae222a73e
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-17 06:36:24 +0900

    add fold()

--- src/main/kotlin/17_04_23.kt
+++ src/main/kotlin/17_04_23.kt
@@ -1,6 +1,13 @@
+/*
 //проблема дня
     fun main() {
         val numbers = mutableListOf(12, 17, 8, 101, 33)
         // do not touch the lines below
         println(numbers.joinToString())
+    }*/
+fun main() {
+    val totalPrice = cookies.fold(0.0) { total, cookie ->
+        total + cookie.price
     }
+    println("Total price: $${totalPrice}")
+}
