2beeb062bfba131c53f70ce2a18e64082f492433
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-17 23:30:15 +0900

    add event

--- src/main/kotlin/17_04_23.kt
+++ src/main/kotlin/17_04_23.kt
@@ -5,11 +5,11 @@
         // do not touch the lines below
         println(numbers.joinToString())
     }*/
-fun main() {
-  /*  val totalPrice = cookies.fold(0.0) { total, cookie ->
+/*fun main() {
+  *//*  val totalPrice = cookies.fold(0.0) { total, cookie ->
         total + cookie.price
     }
-    println("Total price: $${totalPrice}")*/
+    println("Total price: $${totalPrice}")*//*
     val alphabeticalMenu = cookies.sortedBy {
         it.name
     }
@@ -17,4 +17,34 @@ fun main() {
     alphabeticalMenu.forEach() {
         println("${it.name}")
     }
+}*/
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
+enum class Daypart { MORNING, AFTERNOON, EVENING, }
+fun main() {
+    val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+    val events = mutableListOf<Event>(event1, event2, event3, event4, event5, event6)
+    val shortEvents = events.filter { it.durationInMinutes <= 60 }
+    println("You have ${shortEvents.size} short events.")
+    val morningEvent = events.filter { it.daypart == Daypart.MORNING }
+    val afternoonEvent = events.filter { it.daypart == Daypart.AFTERNOON }
+    val eveningEvent = events.filter { it.daypart == Daypart.EVENING }
+    println("Morning: ${morningEvent.size} events")
+    println("Afternoon: ${afternoonEvent.size} events")
+    println("Evening: ${eveningEvent.size} events")
 }
+
+
+
+
+
+
