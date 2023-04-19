931f9140a38b84b55c87f97fdd4f87c573abf8c2
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-19 14:56:22 +0900

    correct

--- src/main/kotlin/19_04_23.kt
+++ src/main/kotlin/19_04_23.kt
@@ -1,18 +1,23 @@
 // повторения с хайпрескил
-class City(val name: String) {
+const val DUBAI = 30
+const val MOSCOW = 5
+const val HANOI = 20
+const val MIN = -92
+const val MAX = 57
+
+data class City(val name: String) {
     var degrees: Int = 0
         set(value) {
-            if ((value < -92) || (value > 57)) {
-                field = when (name) {
-                    "Dubai" -> 30
-                    "Moscow" -> 5
-                    "Hanoi" -> 20
+            field = if (value !in MIN..MAX) {
+                when (name) {
+                    "Dubai" -> DUBAI
+                    "Moscow" -> MOSCOW
+                    "Hanoi" -> HANOI
                     else -> 0
                 }
-            } else field = value
+            } else value
         }
 }
-
 
 fun main() {
     val first = readLine()!!.toInt()
@@ -24,13 +29,12 @@ fun main() {
     secondCity.degrees = second
     val thirdCity = City("Hanoi")
     thirdCity.degrees = third
-    if (firstCity.degrees < secondCity.degrees && firstCity.degrees < thirdCity.degrees) {
-        println(firstCity.name)
-    } else if (secondCity.degrees < firstCity.degrees && secondCity.degrees < thirdCity.degrees) {
-        println(secondCity.name)
-    } else if (thirdCity.degrees < firstCity.degrees && thirdCity.degrees < secondCity.degrees) {
-        println(thirdCity.name)
-    } else {
-        println("neither")
-    }
+    println(
+        when {
+            firstCity.degrees < secondCity.degrees && firstCity.degrees < thirdCity.degrees -> firstCity.name
+            secondCity.degrees < firstCity.degrees && secondCity.degrees < thirdCity.degrees -> secondCity.name
+            thirdCity.degrees < secondCity.degrees && thirdCity.degrees < firstCity.degrees -> thirdCity.name
+            else -> "neither"
+        }
+    )
 }
