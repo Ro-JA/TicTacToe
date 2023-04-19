5605d5e1d653326a929bdc651a3bc88c91fbdc6e
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-19 15:11:23 +0900

    work

--- src/main/kotlin/19_04_23.kt
+++ src/main/kotlin/19_04_23.kt
@@ -1,3 +1,4 @@
+/*
 // повторения с хайпрескил
 const val DUBAI = 30
 const val MOSCOW = 5
@@ -37,4 +38,13 @@ fun main() {
             else -> "neither"
         }
     )
+}*/
+
+//проблема дня
+fun main() {
+    val beyondTheWall = readLine()!!.split(", ").map { it }.toMutableList()
+    val backToTheWall = readLine()!!.split(", ").map { it }.toMutableList()
+    // do not touch the lines above
+    // write your code here
+    println(beyondTheWall.sorted() == backToTheWall.sorted())
 }
