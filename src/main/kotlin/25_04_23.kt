c1127a86080f039f75b505f108cdc15f37b117b4
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-25 06:43:34 +0900

    not work

--- src/main/kotlin/25_04_23.kt
+++ src/main/kotlin/25_04_23.kt
@@ -2,9 +2,14 @@ fun main() {
     val numbers = readLine()!!.split(' ').map { it.toInt() }.toMutableList()
     // Do not touch lines above
     // Write only exchange actions here.
-    numbers[0] = numbers[0] + numbers[numbers.size - 1]
-    numbers[numbers.size - 1] = numbers[0] - numbers[numbers.size - 1]
-    numbers[0] = numbers[0] - numbers[numbers.size - 1]
+    val a = numbers[0]
+    val b = numbers[numbers.size - 1]
+    val c = a
+    numbers[0]= b
+    println(numbers)
+    numbers[numbers.size - 1] = c
+    println(numbers)
+
     // Do not touch lines below
     println(numbers.joinToString(separator = " "))
 }
